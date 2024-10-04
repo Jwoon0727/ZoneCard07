2c9a971c58f23e637374430f0c9fa212d86a9f3d
Jwoon0727 <149401369+Jwoon0727@users.noreply.github.com>
2024-10-04 16:45:28 +0900

    10/4 5

--- app/noticeboard/page.js
+++ app/noticeboard/page.js
@@ -2,37 +2,47 @@
 import ClientNavbar from '../components/ClientNavbar';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import { useState } from 'react';
 
 function JustifiedExample() {
+  const [key, setKey] = useState('profile');
+
   return (
     <div>
-    <ClientNavbar/>
+      <ClientNavbar />
 
-    <Tabs
-      defaultActiveKey="profile"
-      id="justify-tab-example"
-      className="mb-3"
-      justify
-    >
-      <Tab eventKey="home" title="Home">
-        Tab content for Home
-      </Tab>
-      <Tab eventKey="profile" title="Profile">
-        Tab content for Profile
-      </Tab>
-      <Tab eventKey="longer-tab" title="Loooonger Tab">
-        Tab content for Loooonger Tab
-      </Tab>
-      <Tab eventKey="longer-tab1" title="Loooonger Tab1">
-        Tab content for Loooonger Tab
-      </Tab>
-      <Tab eventKey="longer-tab2" title="Loooonger Tab2">
-        Tab content for Loooonger Tab
-      </Tab>
-      <Tab eventKey="contact" title="Contact" >
-        Tab content for Contact
-      </Tab>
-    </Tabs>
+      <div style={{ overflowX: 'auto', whiteSpace: 'nowrap' }}>
+        <Tabs
+          id="justify-tab-example"
+          activeKey={key}
+          onSelect={(k) => setKey(k)}
+          className="mb-3"
+          style={{
+            display: 'inline-flex',
+            whiteSpace: 'nowrap',
+            width: '100%',
+          }}
+        >
+          <Tab eventKey="home" title="Home">
+            Tab content for Home
+          </Tab>
+          <Tab eventKey="profile" title="Profile">
+            Tab content for Profile
+          </Tab>
+          <Tab eventKey="longer-tab" title="Loooonger Tab">
+            Tab content for Loooonger Tab
+          </Tab>
+          <Tab eventKey="longer-tab1" title="Loooonger Tab1">
+            Tab content for Loooonger Tab1
+          </Tab>
+          <Tab eventKey="longer-tab2" title="Loooonger Tab2">
+            Tab content for Loooonger Tab2
+          </Tab>
+          <Tab eventKey="contact" title="Contact">
+            Tab content for Contact
+          </Tab>
+        </Tabs>
+      </div>
     </div>
   );
 }
